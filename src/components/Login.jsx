201a31f7673afc9ae0fa5f493efc759ac0844679
Nazmul Polash <mdpoash01980@gmail.com>
2025-08-04 01:12:59 +0600

    login and register page update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,56 @@
-import { FaGoogle,FaFacebook, FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { FaGoogle, FaFacebook, FaGithub } from "react-icons/fa";
 import { Link } from "react-router";
 
 const Login = () => {
+   const {
+      register,
+      handleSubmit,
+      watch,
+      formState: { errors }, } = useForm();
+   const onSubmit = (data) => console.log(data)
+
    return (
       <div className="flex items-center justify-center  min-h-screen bg-gray-100">
          <div className="w-full max-w-md space-y-6 bg-white shadow-md rounded-lg p-8">
             <h2 className="text-2xl font-bold text-center text-gray-800">Login Page</h2>
 
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
                <div>
                   <label className="block mb-2 text-lg font-semibold text-gray-700">Email:</label>
-                  <input type="email" className={`w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2`} />
+                  <input
+                     type="email"
+                     className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2"
+                     {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                           value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                           message: "Please enter a valid email address"
+                        }
+                     })}
+                  />
+                  {errors.email && <p className="text-sm italic text-red-500">{errors.email.message}</p>}
                </div>
                <div>
                   <label className="block mb-2 text-lg font-semibold text-gray-700">Password:</label>
-                  <input type="password" className={`w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2`} />
+                  <input
+                     type="password"
+                     className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2"
+                     {...register("password", {
+                        required: "Password is required",
+                        minLength: {
+                           value: 8,
+                           message: "Password must be at least 8 characters"
+                        }
+                     })}
+                  />
+                  {errors.password && <p className="text-sm italic text-red-500">{errors.password.message}</p>}
                </div>
                <div>
                   <button type="submit" className="btn btn-primary w-full rounded-md"> Login  </button>
                </div>
             </form>
+
             <div className="text-center space-y-4">
                <p className="text-gray-600 ">Or sign up with</p>
                <div className="space-x-4 flex justify-center">
@@ -28,7 +59,7 @@ const Login = () => {
                   <button className="btn btn-info"><FaFacebook /> <span>Facebook</span></button>
                </div>
             </div>
-            <p className="text-center font-semibold">You have no account ? Plesas <Link to="/reginster" className="text-blue-700">Register.</Link> </p>
+            <p className="text-center font-semibold">You have no account ? Plesas <Link to="/register" className="text-blue-700">Register.</Link> </p>
          </div>
 
       </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,27 +1,69 @@
+import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router";
+import { useForm } from "react-hook-form"
 
 
 const Register = () => {
+
+   const {
+      register,
+      handleSubmit,
+      watch,
+      formState: { errors },
+   } = useForm()
+
+   const onSubmit = (data) => console.log(data)
+
+
    return (
       <div className="flex items-center justify-center h-screen bg-gray-100">
          <div className="bg-white p-8 rounded-lg shadow-md w-96">
             <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
-            <form>
+
+            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
                <div className="mb-4">
                   <label className="block text-sm font-medium mb-2" htmlFor="name">Name</label>
-                  <input type="text" id="name" className="w-full p-2 border border-gray-300 rounded" required />
+                  <input type="text" id="name" className="w-full p-2 border border-gray-300 rounded"
+                     {...register("name", { required: "Name is required" })}
+                  />
+                  {errors.name && (<p className="text-sm text-red-600 italic">{errors.name.message}</p>)}
                </div>
+
                <div className="mb-4">
                   <label className="block text-sm font-medium mb-2" htmlFor="email">Email</label>
-                  <input type="email" id="email" className="w-full p-2 border border-gray-300 rounded" required />
+                  <input type="email" id="email" className="w-full p-2 border border-gray-300 rounded"
+                     {...register("email", { required: "Email is required", pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "Please entered a correct email" } })} />
+                  {errors.email && (<p className="text-sm text-red-600 italic">{errors.email.message}</p>)}
                </div>
                <div className="mb-4">
                   <label className="block text-sm font-medium mb-2" htmlFor="password">Password</label>
-                  <input type="password" id="password" className="w-full p-2 border border-gray-300 rounded" required />
+                  <input type="password" id="password" className="w-full p-2 border border-gray-300 rounded"
+                     {...register("password", { required: "Password is required", pattern: { value: 8, message: "Please entered at least 8 charecters" } })} />
+                  {errors.password && (<p className="text-sm text-red-600 italic">{errors.password.message}</p>)}
                </div>
+               <div className="mb-4">
+                  <label className="block text-sm font-medium mb-2" htmlFor="confirmPassword">Confirm Password</label>
+                  <input type="password" id="confirmPassword" className="w-full p-2 border border-gray-300 rounded"
+                     {...register("confirm_password", { required: "Confirm Password is required", minLength: { value: 8, message: "Please enter at least 8 characters" },
+                        validate: (value) => value === watch("password") || "Passwords do not match" })}/>
+                  {errors.confirm_password && ( <p className="text-sm text-red-600 italic">{errors.confirm_password.message}</p>)}
+               </div>
+
                <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-200">Register</button>
             </form>
+
+            <div className="text-center space-y-4">
+               <p className="text-gray-600 mt-5">Or sign up with</p>
+               <div className="space-x-4 flex justify-center">
+                  <button className="btn btn-secondary"><FaGoogle /><span>Google</span></button>
+                  <button className="btn btn-neutral"> <FaGithub /> <span>GitHub</span></button>
+                  <button className="btn btn-info"><FaFacebook /> <span>Facebook</span></button>
+               </div>
+            </div>
+
+            <p className="text-center font-semibold mt-5">Already have an account ? Plesas <Link to="/login" className="text-blue-700">Login.</Link> </p>
          </div>
       </div>
    );
-}  
+}
 export default Register;
